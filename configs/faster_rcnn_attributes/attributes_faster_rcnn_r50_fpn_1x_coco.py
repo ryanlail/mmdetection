5cb2b0b078cc5b8323db7f19767c3a46b9815692
Ryan Lail <me@ryanlail.com>
2021-01-24 21:32:55 +0000

    initial config

---
+++ configs/faster_rcnn_attributes/attributes_faster_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,12 @@
+
+_base_ = '../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+model = dict(
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='AttributeBBoxHead')))
