50368ee8d2d7874f2320e9c61acd33a04f586ea5
Ryan Lail <me@ryanlail.com>
2021-01-25 09:35:43 +0000

    try to modify helpers to deal with 5 outputs

--- mmdet/models/roi_heads/bbox_heads/attribute_bbox_head.py
+++ mmdet/models/roi_heads/bbox_heads/attribute_bbox_head.py
@@ -315,14 +315,14 @@ class AttributeBBoxHead(nn.Module):
                 losses['loss_bbox'] = bbox_pred[pos_inds].sum()
         return losses
     
-    @force_fp32(apply_to=('cls_score', 'bbox_pred'))
+    @force_fp32(apply_to=('cls_score', 'bbox_pred', 'face_score', 'colour_score', 'motion_score'))
     def get_bboxes(self,
                    rois,
                    cls_score,
                    bbox_pred,
-                   #face_score,#
-                   #colour_score,#
-                   #motion_score,#
+                   face_score,#
+                   colour_score,#
+                   motion_score,#
                    img_shape,
                    scale_factor,
                    rescale=False,
@@ -331,6 +331,18 @@ class AttributeBBoxHead(nn.Module):
             cls_score = sum(cls_score) / float(len(cls_score))
         scores = F.softmax(cls_score, dim=1) if cls_score is not None else None
 
+        if isinstance(face_score, list):
+            face_score = sum(face_score) / float(len(face_score))
+        face_scores = F.softmax(face_score, dim=1) if face_score is not None else None
+        
+        if isinstance(colour_score, list):
+            colour_scores = sum(colour_score) / float(len(colour_score))
+        face_scores = F.softmax(cls_score, dim=1) if colour_score is not None else None
+        
+        if isinstance(motion_score, list):
+            motion_score = sum(motion_score) / float(len(motion_score))
+        motion_scores = F.softmax(motion_score, dim=1) if motion_score is not None else None
+        
         if bbox_pred is not None:
             bboxes = self.bbox_coder.decode(
                 rois[:, 1:], bbox_pred, max_shape=img_shape)
@@ -349,9 +361,9 @@ class AttributeBBoxHead(nn.Module):
                           scale_factor).view(bboxes.size()[0], -1)
 
         if cfg is None:
-            return bboxes, scores
+            return bboxes, scores, face_scores, colour_scores, motion_scores
         else:
-            det_bboxes, det_labels = multiclass_nms(bboxes, scores,
+            det_bboxes, det_labels, det_faces, det_colours, det_motions = multiclass_nms(bboxes, scores,
                                                     cfg.score_thr, cfg.nms,
                                                     cfg.max_per_img)
 
@@ -464,3 +476,83 @@ class AttributeBBoxHead(nn.Module):
             new_rois = torch.cat((rois[:, [0]], bboxes), dim=1)
 
         return new_rois
+
+def multiclass_nms(multi_bboxes,
+                   multi_scores,
+                   multi_faces,
+                   multi_colours,
+                   multi_motions,
+                   score_thr,
+                   nms_cfg,
+                   max_num=-1,
+                   score_factors=None,
+                   return_inds=False):
+    """NMS for multi-class bboxes.
+
+    Args:
+        multi_bboxes (Tensor): shape (n, #class*4) or (n, 4)
+        multi_scores (Tensor): shape (n, #class), where the last column
+            contains scores of the background class, but this will be ignored.
+        score_thr (float): bbox threshold, bboxes with scores lower than it
+            will not be considered.
+        nms_thr (float): NMS IoU threshold
+        max_num (int, optional): if there are more than max_num bboxes after
+            NMS, only top max_num will be kept. Default to -1.
+        score_factors (Tensor, optional): The factors multiplied to scores
+            before applying NMS. Default to None.
+        return_inds (bool, optional): Whether return the indices of kept
+            bboxes. Default to False.
+
+    Returns:
+        tuple: (bboxes, labels, indices (optional)), tensors of shape (k, 5),
+            (k), and (k). Labels are 0-based.
+    """
+    num_classes = multi_scores.size(1) - 1
+    # exclude background category
+    if multi_bboxes.shape[1] > 4:
+        bboxes = multi_bboxes.view(multi_scores.size(0), -1, 4)
+    else:
+        bboxes = multi_bboxes[:, None].expand(
+            multi_scores.size(0), num_classes, 4)
+
+    scores = multi_scores[:, :-1]
+
+    labels = torch.arange(num_classes, dtype=torch.long)
+    labels = labels.view(1, -1).expand_as(scores)
+
+    bboxes = bboxes.reshape(-1, 4)
+    scores = scores.reshape(-1)
+    labels = labels.reshape(-1)
+
+    # remove low scoring boxes
+    valid_mask = scores > score_thr
+    # multiply score_factor after threshold to preserve more bboxes, improve
+    # mAP by 1% for YOLOv3
+    if score_factors is not None:
+        # expand the shape to match original shape of score
+        score_factors = score_factors.view(-1, 1).expand(
+            multi_scores.size(0), num_classes)
+        score_factors = score_factors.reshape(-1)
+        scores = scores * score_factors
+    inds = valid_mask.nonzero(as_tuple=False).squeeze(1)
+    bboxes, scores, labels = bboxes[inds], scores[inds], labels[inds]
+    if inds.numel() == 0:
+        if torch.onnx.is_in_onnx_export():
+            raise RuntimeError('[ONNX Error] Can not record NMS '
+                               'as it has not been executed this time')
+        if return_inds:
+            return bboxes, labels, inds
+        else:
+            return bboxes, labels
+
+    # TODO: add size check before feed into batched_nms
+    dets, keep = batched_nms(bboxes, scores, labels, nms_cfg)
+
+    if max_num > 0:
+        dets = dets[:max_num]
+        keep = keep[:max_num]
+
+    if return_inds:
+        return dets, labels[keep], keep
+    else:
+        return dets, labels[keep]
